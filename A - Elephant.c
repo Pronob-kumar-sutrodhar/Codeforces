fae1b28de73e9b843a1f5af57d4bc92b2df77019
PRONOB KUMAR SUTRODHAR <kpronob74@gmail.com>
2025-07-15 14:53:29 +0600

    Create A - Elephant.c

---
+++ A - Elephant.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int x;
+    scanf("%d", &x);
+ 
+    int steps = x / 5;
+    if (x % 5 != 0)
+        steps += 1;
+ 
+    printf("%d\n", steps);
+    return 0;
+}
