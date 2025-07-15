bf8c18253fae20e249e8471387f31d6f4512e725
PRONOB KUMAR SUTRODHAR <kpronob74@gmail.com>
2025-07-15 14:52:18 +0600

    Create A - Stones on the Table.c

---
+++ A - Stones on the Table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char s[n+1];
+     scanf("%s", s);
+ 
+   int count=0;
+ 
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            count++;
+        }
+    
+      
+      
+   }
+  printf("%d",count);
+    return 0;
+}
